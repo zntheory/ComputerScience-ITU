b86b2d39b088e223f2a44e50cb7bdadce7dffab0
zntheory <40925511+zntheory@users.noreply.github.com>
2025-09-30 18:01:30 +0200

    feat: Make Walrus Weights acceptable.
    Co-authored-by: TB <thor.hoegsbro@gmail.com>

--- ALDE/manda4/walrusWeights.py
+++ ALDE/manda4/walrusWeights.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python
 
+no_plates = int(input())
+
+plates = []
+for _ in range(no_plates):
+    plates.append(int(input()))
+
+max_weight = 2000
+table = [[False for _ in range(max_weight + 1)] for _ in range(no_plates + 1)]
+
+for i in range(no_plates + 1):
+    table[i][0] = True
+
+for i in range(1, no_plates + 1):
+    plate_weight = plates[i - 1]
+
+    for w in range(0, max_weight + 1):
+        drop = table[i-1][w]
+        take = False
+        if plate_weight <= w:
+            take = table[i - 1][w - plate_weight]
+
+        table[i][w] = drop | take
+
+intended_weight = 1000
+
+if table[no_plates][intended_weight]:
+    print(intended_weight)
+    exit(0)
+
+for i in range(intended_weight + 1):
+    if table[no_plates][intended_weight + i]:
+        print(intended_weight + i)
+        exit(0)
+    if table[no_plates][intended_weight - i]:
+        print(intended_weight - i)
+        exit(0)
